16723724dc7459c7c9e92357d59104510dbe9ad8
[AI 10]임도헌 <ldh2233@gmail.com>
2024-07-22 18:48:03 +0900

    📰 New: edit 페이지 생성

---
+++ src/app/(edit)/edit/page.tsx
@@ -0,0 +1,21 @@
+/**
+File Name : app/edit/page
+Description : 동화 편집 페이지
+Author : 임도헌
+
+History
+Date        Author   Status    Description
+2024.07.22  임도헌    Created
+*/
+
+import FairytailInfo from '@/components/edit/fairytail-info';
+import FairytailStory from '@/components/edit/fairytail-story';
+
+export default function editPage() {
+    return (
+        <div className="flex w-full">
+            <FairytailInfo />
+            <FairytailStory />
+        </div>
+    );
+}
