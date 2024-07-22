69c448a4422aac5a117e6b828dec68c8cee6a88a
[AI 10]임도헌 <ldh2233@gmail.com>
2024-07-22 19:01:45 +0900

    📰 New: book-preview페이지 생성

---
+++ src/app/(edit)/edit/book-preview/page.tsx
@@ -0,0 +1,17 @@
+/**
+File Name : app/edit/book-preview/page
+Description : 동화책 미리보기 및 편집 페이지
+Author : 임도헌
+
+History
+Date        Author   Status    Description
+2024.07.22  임도헌    Created
+*/
+
+export default function BookPreviewPage() {
+    return (
+        <div className="flex w-full">
+            <div>동화책 커스텀 화면입니다.</div>
+        </div>
+    );
+}
