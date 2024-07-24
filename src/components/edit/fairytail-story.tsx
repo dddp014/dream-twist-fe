c84e19a7cc946c626c51b08db43ed8a373642620
[AI 10]임도헌 <ldh2233@gmail.com>
2024-07-24 23:05:14 +0900

    ✨Feat : Toggle 컴포넌트 분리 및 생성

--- src/components/edit/fairytail-story.tsx
+++ src/components/edit/fairytail-story.tsx
@@ -11,6 +11,7 @@ Date        Author   Status    Description
 
 import Link from 'next/link';
 import { useState } from 'react';
+import Toggle from '../common/Toggle';
 
 export default function FairytailStory() {
     const [isClick, setIsClick] = useState(0);
@@ -46,17 +47,7 @@ export default function FairytailStory() {
             className="w-8/12 m-4 mt-24 p-2 pl-1 border border-green-300 rounded-lg shadow"
         >
             <div className="flex justify-end">
-                <label className="inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" className="sr-only peer" />
-                    <div
-                        className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300
-                rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full
-                peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px]
-                after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all
-                peer-checked:bg-green-600"
-                    ></div>
-                    <span className="ms-3 text-sm font-medium">공개 여부</span>
-                </label>
+                <Toggle />
                 <button className="bg-main py-2 px-4 ml-2 rounded-[7px] text-white cursor-pointer text-base">
                     저장
                 </button>
