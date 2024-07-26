e9839a5081f20763138748e7d12db8474165459c
[AI10] 나경윤 <skruddbs99@naver.com>
2024-07-26 17:31:20 +0900

    💄 Design: 로그인 css 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,21 +13,13 @@ import Link from 'next/link';
 
 export default function Login() {
     return (
-        <div className="flex flex-col items-center justify-center h-full bg-main-100">
-            <div className="relative w-full -mt-[26rem]">
-                <Image
-                    src="/images/background-pattern.svg"
-                    alt="logo"
-                    width={1920}
-                    height={0}
-                />
-            </div>
-            <div className="flex flex-col items-center justify-center absolute mb-36">
+        <div className="relative flex flex-col items-center justify-center h-full">
+            <div className="flex flex-col items-center justify-center mb-36">
                 <Link href="/" className="cursor-pointer">
                     <Image
                         src="/images/logo.svg"
                         alt="logo"
-                        width={180}
+                        width={200}
                         height={0}
                         className=""
                     />
@@ -35,7 +27,9 @@ export default function Login() {
                 <span className="text-sm text-gray-700">
                     Ai 동화 생성 서비스
                 </span>
-                <p className="text-2xl text-main font-semibold my-7">로그인</p>
+                <p className="text-2xl text-main font-semibold mt-8 mb-7 ">
+                    로그인
+                </p>
                 <button
                     type="button"
                     className="flex flex-row items-center justify-center w-72 border border-gray-300 rounded-lg py-3 hover:bg-gray-50"
@@ -48,6 +42,7 @@ export default function Login() {
                     />
                     <span className="ml-4">Google로 시작하기</span>
                 </button>
+                <div className="absolute bottom-0 w-full bg-main-100 h-12"></div>
             </div>
         </div>
     );
