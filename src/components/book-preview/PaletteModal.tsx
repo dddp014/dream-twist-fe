4401f57868dcf55108a50415542212c52156ca20
[AI 10]임도헌 <ldh2233@gmail.com>
2024-07-28 16:53:16 +0900

    ✨ Feat: 이미지 선택 모달 추가

---
+++ src/components/book-preview/PaletteModal.tsx
@@ -0,0 +1,13 @@
+/**
+File Name : compoenents/book-preview/PaletteModal
+Description : 이미지 첨부에서 그림판 사용 시
+Author : 임도헌
+
+History
+Date        Author   Status    Description
+2024.07.26  임도헌    Created
+*/
+
+export default function PaletteModal() {
+    return <div></div>;
+}
