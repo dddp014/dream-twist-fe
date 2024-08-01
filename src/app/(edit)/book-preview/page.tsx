0019738ff369f41d74610b0bbf35421d0b184b19
[AI 10]임도헌 <ldh2233@gmail.com>
2024-08-01 15:02:59 +0900

    🔨 Refactor: 코드 리팩토링 Portal 위치 수정 및 arrowIcon 카멜케이스로 수정

--- src/app/(edit)/book-preview/page.tsx
+++ src/app/(edit)/book-preview/page.tsx
@@ -7,6 +7,9 @@ History
 Date        Author   Status    Description
 2024.07.22  임도헌   Created
 2024.07.27  임도헌   Modified  Portal기술 적용
+2024.07.30  임도헌   Modified  jotai 적용
+2024.07.31  임도헌   Modified  react-hook-form으로 코드 변경 및 portal 위치 변경(app/layout.tsx로 옮김)
+
 */
 
 import Book from '@/components/book-preview/Book';
@@ -15,7 +18,6 @@ export default function BookPreviewPage() {
     return (
         <div>
             <Book />
-            <div id="modal-root" />
         </div>
     );
 }
--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -4,10 +4,11 @@ Description : modal 관련 기능
 Author : 김민규, 임도헌
 
 History
-Date        Author   Status    Description
-2024.07.24  김민규    Created  useModal 생성
-2024.07.24  임도헌    Created  useEditModal생성
-2024.07.27  임도헌    Modified useEditModal파일을 useModal로 합침
+Date        Author   Status     Description
+2024.07.24  김민규    Created   useModal 생성
+2024.07.24  임도헌    Created   useEditModal생성
+2024.07.27  임도헌    Modified  useEditModal파일을 useModal로 합침
+2024.07.31  임도헌    Modified  함수 type 명시
 */
 
 // hooks/useModal.ts
@@ -35,14 +36,14 @@ const useModal = (initialPlot: string) => {
 };
 
 function useEditModal() {
-    const [isOpenModal, setIsOpenModal] = useState(false);
-    const [isSecondOpenModal, setIsSecondOpenModal] = useState(false);
+    const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+    const [isSecondOpenModal, setIsSecondOpenModal] = useState<boolean>(false);
 
-    const openModal = () => setIsOpenModal(true);
-    const closeModal = () => setIsOpenModal(false);
+    const openModal = (): void => setIsOpenModal(true);
+    const closeModal = (): void => setIsOpenModal(false);
 
-    const secondOpenModal = () => setIsSecondOpenModal(true);
-    const secondCloseModal = () => setIsSecondOpenModal(false);
+    const secondOpenModal = (): void => setIsSecondOpenModal(true);
+    const secondCloseModal = (): void => setIsSecondOpenModal(false);
 
     return {
         isOpenModal,
