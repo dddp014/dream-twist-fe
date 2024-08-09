4affa8e70127bfaf0b895bb9794a59c41a7e3267
[AI10] 나경윤 <skruddbs99@naver.com>
2024-08-10 04:35:36 +0900

    🔥 Remove: confing 코드 제거

--- next.config.mjs
+++ next.config.mjs
@@ -22,13 +22,6 @@ const nextConfig = {
         // your project has ESLint errors.
         ignoreDuringBuilds: true
     },
-    redirects: async () => [
-        {
-            source: '/auth/google/callback',
-            destination: '/callback', // 콜백 페이지로 리디렉션
-            permanent: false
-        }
-    ],
     reactStrictMode: false
 };
 
